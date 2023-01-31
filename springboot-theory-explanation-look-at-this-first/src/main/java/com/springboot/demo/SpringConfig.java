6ccf482078fd52c47a0543442e05460b46f1af7e
Prateek Sharma <prateekshrmprateek@gmail.com>
2023-02-01 00:02:53 +0530

    spring-boot concept done

---
+++ springboot-theory-explanation-look-at-this-first/src/main/java/com/springboot/demo/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.springboot.demo;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+	// using @configuration annotation we can make a normal class a spring java based configuration class
+	// it can be used to create beans
+}
