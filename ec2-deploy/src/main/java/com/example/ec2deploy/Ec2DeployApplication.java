b8a601b4d23e003715f87d4e1e01fe984fa6bb82
Prateek Sharma <prateekshrmprateek@gmail.com>
2023-04-26 00:19:58 +0530

    deploy spring-boot app

---
+++ ec2-deploy/src/main/java/com/example/ec2deploy/Ec2DeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec2deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2DeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2DeployApplication.class, args);
+	}
+
+}
---
+++ ec2-deploy/src/test/java/com/example/ec2deploy/Ec2DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2DeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
